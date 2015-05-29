3295d55465f6779e3222481e5fa7d39b69444e7d
Luke Hedger <luke.hedger@gmail.com>
2015-05-29 15:36:06 +0100

    sort diet server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = require('diet'),
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000
 
-app.listen(server_ip_address + ":" + server_port)
+app.listen("http://" + server_ip_address + ":" + server_port)
 
 app.get('/', function($){
     $.end('Hello World!')
