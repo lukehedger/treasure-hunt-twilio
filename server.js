a2eba03c427ac0795cf4009a2923632fb29cec21
Luke Hedger <luke.hedger@gmail.com>
2015-06-01 11:35:58 +0100

    forever

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello Twilio!');
+    console.log("hello...?");
 });
 
 app.post('/message', function (req, res) {
 
-    console.log(req.body);
-
     var resp = new twilio.TwimlResponse();
 
     // TODO - dynamic message based on clue #
+    console.log("body:", req.body);
     // store clues in config.json file clues: { 1: { hint: "this is a hint" } }
     // get clue number from text body eg. CLUE 1
     // then get config.clues[1].hint and resp.message(hint);
@@ -26,7 +26,6 @@ app.post('/message', function (req, res) {
 
     res.set('Content-Type', 'text/xml');
 
-    // TODO - does this send a response sms automatically or do we need to use client.sendSms ?
     res.send(resp.toString());
 });
 
@@ -36,18 +35,3 @@ var server = app.listen(server_port, server_ip_address, function () {
 
     console.log( "Listening on http://" + host + ":" + port );
 });
-
-// client.sendSms({
-//     to:'447964209427',
-//     from:'441329801049',
-//     body:'ahoy hoy! Testing Twilio and node.js'
-// }, function(error, message) {
-//     if (!error) {
-//         console.log('Success! The SID for this SMS message is:');
-//         console.log(message.sid);
-//         console.log('Message sent on:');
-//         console.log(message.dateCreated);
-//     } else {
-//         console.log('Oops! There was an error.');
-//     }
-// });
