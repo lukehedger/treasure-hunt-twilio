2ff0ea9f559d27782fa5ba5fc4a8cca0f740b102
Luke Hedger <luke.hedger@gmail.com>
2015-06-01 10:28:43 +0100

    test

--- server.js
+++ server.js
@@ -31,15 +31,18 @@
 var express = require('express');
 var app = express();
 
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(server_port, server_ip_address, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log( "Listening on " + server_ip_address + ", server_port " + port );
 
 });
